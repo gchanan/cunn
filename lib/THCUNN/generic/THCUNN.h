fd2351350df47f027af4877f4e70008a4fe7cde5
Gregory Chanan <gchanan@fb.com>
2016-10-31 11:39:50 -0700

    Add generic support for VolumetricDilatedMaxPooling.
    Doesn't include test changes.

--- lib/THCUNN/generic/THCUNN.h
+++ lib/THCUNN/generic/THCUNN.h
@@ -914,6 +914,27 @@ TH_API void THNN_(Threshold_updateGradInput)(
                   real val,
                   bool inplace);
 
+TH_API void THNN_(VolumetricDilatedMaxPooling_updateOutput)(
+                  THCState *state,
+                  THCTensor *input,
+                  THCTensor *output,
+                  THCIndexTensor *indices,
+                  int kT, int kW, int kH,
+                  int dT, int dW, int dH,
+                  int padT, int padW, int padH,
+                  int dilationT, int dilationW, int dilationH,
+                  bool ceilMode);
+
+TH_API void THNN_(VolumetricDilatedMaxPooling_updateGradInput)(
+                  THCState *state,
+                  THCTensor *input,
+                  THCTensor *gradOutput,
+                  THCTensor *gradInput,
+                  THCIndexTensor *indices,
+                  int dT, int dW, int dH,
+                  int padT, int padW, int padH,
+                  int dilationT, int dilationW, int dilationH);
+
 TH_API void THNN_(VolumetricMaxUnpooling_updateOutput)(
                   THCState *state,
                   THCTensor *input,
